25f482bbae85c82aed99f7ea7207b6fa98129cee
Helen Kent <helen8297@gmail.com>
2020-06-26 14:56:20 +0100

    google maps stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import GoogleMaps from "../Map";
 
 function App() {
   return (
     <div className="App">
       <h2> Food Hackathon (Working Title) </h2>
+      <GoogleMaps />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const MAPAPIKEY = process.env.REACT_APP_MAPS_API_KEY;
