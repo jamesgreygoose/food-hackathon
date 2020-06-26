c4e7fc27680b2682b15d08332b668b88edd27912
Mell <mellkayrusson@gmail.com>
2020-06-26 15:42:25 +0100

    fixes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import GoogleMaps from "../Map";
-=======
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
->>>>>>> 687b8f45b5362caae37d794f4d72e25e4b0285a7
 import HomePage from "../Home";
 import StayingIn from "../StayingIn";
 import GoingOut from "../GoingOut";
@@ -26,10 +19,6 @@ function App() {
         </Route>
         <Route path="/goingout">
           <GoingOut />
-<<<<<<< HEAD
-          <GoogleMaps />
-=======
->>>>>>> 687b8f45b5362caae37d794f4d72e25e4b0285a7
         </Route>
         <Route path="/">
           <HomePage />
