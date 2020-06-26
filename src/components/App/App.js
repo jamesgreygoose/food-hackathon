c7fc6ad24d1a1ad73ccc08f77ce7a483361901de
Helen Kent <helen8297@gmail.com>
2020-06-26 14:57:22 +0100

    helen stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+<<<<<<< HEAD
 import GoogleMaps from "../Map";
 
 function App() {
@@ -8,6 +9,33 @@ function App() {
       <h2> Food Hackathon (Working Title) </h2>
       <GoogleMaps />
     </div>
+=======
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "../Home";
+import StayingIn from "../StayingIn";
+import GoingOut from "../GoingOut";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/" className="links">
+          <h2 id="gisMeDinner"> GIS ME DINNER</h2>
+        </Link>
+      </div>
+      <Switch>
+        <Route path="/stayingin">
+          <StayingIn />
+        </Route>
+        <Route path="/goingout">
+          <GoingOut />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+>>>>>>> 9d5de2038e8430566fbb4a21b7709a0f6932f49c
   );
 }
 
---
+++ src/components/GoingOut/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./GoingOut.css";
+
+const initialState = {
+  people: null,
+  cost: null,
+  perc: null,
+};
+
+function GoingOut() {
+  const [bill, setBill] = useState(initialState);
+  const [splitTotal, setSplitTotal] = useState(null);
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    const newName = e.target.name;
+    setBill({ ...bill, [newName]: newValue });
+  }
+
+  function calculateBill() {
+    const cost = bill.cost;
+    const diners = bill.people;
+    const perc = bill.perc;
+    const tip = (cost / 100) * perc;
+    const total = Number(cost) + Number(tip);
+    const payment = total / diners;
+    // console.log("tip:", tip, "total:", typeof total, "payment:", payment);
+    setSplitTotal(payment);
+  }
+
+  return (
+    <>
+      <div>
+        <h2 id="goingOutTitle">Going Out</h2>
+        <div id="whereToGo">
+          <h4>Where do you fancy going?</h4>
+          <input placeholder="Where do you want to go?" />
+          <div id="map"> </div>
+        </div>
+        <div id="billCalculator">
+          <h4>Bill Calculator</h4>
+          <input
+            placeholder="How many people?"
+            name="people"
+            value={bill.people}
+            onChange={handleChange}
+          ></input>
+          <input
+            placeholder="Total cost"
+            name="cost"
+            value={bill.cost}
+            onChange={handleChange}
+          ></input>
+          <input
+            placeholder="Percentage gratuity"
+            name="perc"
+            value={bill.perc}
+            onChange={handleChange}
+          ></input>
+          {bill.people && bill.perc && bill.cost && (
+            <button onClick={calculateBill}>Calculate</button>
+          )}
+          <h4 id="eachPay">You each pay</h4>
+          <p className="p">£{splitTotal}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GoingOut;
